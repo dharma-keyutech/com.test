13df121ff2f6c7d9d5a03c32fb76cfc243a3f1c6
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2020-12-10 11:10:30 +0530

    Update BASE_Class.java

--- src/test/java/com/base/BASE_Class.java
+++ src/test/java/com/base/BASE_Class.java
@@ -28,7 +28,7 @@ public class BASE_Class {
   		 //WebDriverManager.firefoxdriver().setup();
 			 //driver = new FirefoxDriver();
 		// WebDriver driver= new ChromeDriver(new ChromeDriverService.Builder().usingPort(65530).build());
-                   		System.setProperty("webdriver.chrome.driver","/home/teja/chromedriver");
+                   		System.setProperty("webdriver.chrome.driver","home/teja/chromedriver");
 			          ChromeOptions options=new ChromeOptions();
 		                  options.addArguments("headless");
 			          driver=new ChromeDriver(options);
