da1b700e059db8d4aa9bfe12eade1db67d6daec2
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2020-12-10 23:19:07 +0530

    Update BASE_Class.java

--- src/test/java/com/base/BASE_Class.java
+++ src/test/java/com/base/BASE_Class.java
@@ -22,10 +22,10 @@ public class BASE_Class {
 			// location
 			
 	// System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/chromedriver.exe");
-            	//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/chromedriver");
+            System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/chromedriver");
 
-		WebDriverManager.chromedriver().setup();
-		WebDriverManager.chromedriver().browserVersion("87.0.4280.88");
+		//WebDriverManager.chromedriver().setup();
+		//WebDriverManager.chromedriver().browserVersion("87.0.4280.88");
 
 			// driver = new ChromeDriver();
                      // WebDriver driver= new ChromeDriver(new ChromeDriverService.Builder().usingPort(65530).build());
