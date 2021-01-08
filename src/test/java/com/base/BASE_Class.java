b52546d8b76f927fb3dd228f2b5fdcbed47b783a
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2021-01-08 13:07:16 +0530

    Update BASE_Class.java

--- src/test/java/com/base/BASE_Class.java
+++ src/test/java/com/base/BASE_Class.java
@@ -62,7 +62,7 @@ public class BASE_Class {
 			//System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/chromedriver");
 		
 		          // System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/chromedriver");
-			System.setProperty("webdriver.chrome.driver","/var/jenkins_home/workspace/Sample_Testing_Job/drivers/chromedriver");
+			System.setProperty("webdriver.chrome.driver","/var/jenkins_home/workspace/Sample_Testing_Job/drivers/chromedriver.exe");
 		            
 		     //  WebDriverManager.chromedriver().setup();
 	// WebDriverManager.chromedriver().browserVersion("87.0.4280.88");
