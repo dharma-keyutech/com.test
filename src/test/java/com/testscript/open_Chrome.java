d2cc57f5d10878dcc74b6222cf3c31a820a00945
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2021-01-20 17:17:22 +0530

    Update open_Chrome.java

--- src/test/java/com/testscript/open_Chrome.java
+++ src/test/java/com/testscript/open_Chrome.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 import com.base.BASE_Class;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class open_Chrome extends BASE_Class {
 	
@@ -31,30 +34,37 @@ public class open_Chrome extends BASE_Class {
 //		
 	public static WebDriver driver;
 	static String Url="https://www.google.com/";
-	static String exePATH= System.getProperty("user.dir")+"/drivers/chromedriver";
+	static String exePATH= System.getProperty("user.dir")+"/drivers/geckodriver_MAC";
 	@Test
 	public static void  test() throws Throwable    {
 		//Process pr = rt.exec("sudo chmod 777 > exePATH\"");
 
    // System.setProperty("webdriver.gecko.driver",exePATH);
-		System.setProperty("webdriver.chrome.driver",exePATH);
-		// System.setProperty("webdriver.gecko.driver",exePATH);
+		//System.setProperty("webdriver.chrome.driver",exePATH);
+	//	System.setProperty("webdriver.gecko.driver",exePATH);
     //driver = new ChromeDriver();
-	//FirefoxOptions options=new FirefoxOptions();
-	//options.addArguments("--headless");
+//	FirefoxOptions options=new FirefoxOptions();
+//	options.addArguments("--headless");
 		
 //		DesiredCapabilities capabilities=DesiredCapabilities.firefox();
 //		System.out.println("Debug 2");
 //		capabilities.setCapability("marionette", true);
 //		System.out.println("Debug 3");
 //		WebDriver driver = new FirefoxDriver(capabilities);
-	//WebDriver drive= new FirefoxDriver(options);
-		WebDriver drive= new ChromeDriver();
-		System.out.println("Debug 4");
-		driver.get("http://www.google.com");
-		driver.manage().window().maximize();  
-		driver.quit();
+//	WebDriver drive= new FirefoxDriver(options);
+//		System.out.println("Debug 4");
+//		driver.get("http://www.google.com");
+//		driver.manage().window().maximize();  
+//		driver.quit();
 
+	
+WebDriverManager.chromedriver().setup();
+ChromeOptions options=new ChromeOptions();
+options.addArguments("--headless");
+driver = new ChromeDriver(options);
+driver.get("http://www.google.com");
+driver.manage().window().maximize();  
+driver.quit();
 		
 		
 	}
@@ -62,4 +72,5 @@ public class open_Chrome extends BASE_Class {
 	
 	
 	}
+
 
