0332d38118555ae957e9e31301d5f6dc08d76228
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2021-01-20 13:58:40 +0530

    Update open_Chrome.java

--- src/test/java/com/testscript/open_Chrome.java
+++ src/test/java/com/testscript/open_Chrome.java
@@ -1,43 +1,41 @@
 package com.testscript;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.WebDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 
 import com.base.BASE_Class;
 
 public class open_Chrome extends BASE_Class {
 	
-          	public static WebDriver driver;
 
-	// static String Url="https://www.google.com/";
-	@org.testng.annotations.Test
-	public static void  Test() {
-		
-		  System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/ geckodriver");
-		
-		// WebDriverManager.chromedriver().setup();
-	     //  ChromeOptions options=new ChromeOptions();
-              // options.addArguments("headless");
-	         // driver=new ChromeDriver(options);
-		      driver = new FirefoxDriver();
-		//driver = new ChromeDriver();
-                driver.get("http://www.google.com");
+//	// static String Url="https://www.google.com/";
+//	@org.testng.annotations.Test
+//	public static void  Test() {
+//
+//		launchBrowser("https://mail.google.com/mail/u/0/#inbox");
+//       // driver.navigate().to("https://mail.google.com/mail/u/0/#inbox");
+//		
+//		String Tittle= driver.getTitle();
+//		
+//		System.out.println(Tittle);
+//		
+//		
+//		String Source =driver.getPageSource();
+//		// System.out.println(Source);
+//		
+	public static WebDriver driver;
+	static String Url="https://www.google.com/";
+	@Test
+	public static void  test() {
+    System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/drivers/chromedriver");
+    driver = new ChromeDriver();
+	driver.get(Url);
 
-		//driver.navigate().to("https://www.google.com/");
-
-		// launchBrowser("https://mail.google.com/mail/u/0/#inbox");
-       // driver.navigate().to("https://mail.google.com/mail/u/0/#inbox");
-		
-		
 		
 		
 	}
+
 	
 	}
 
