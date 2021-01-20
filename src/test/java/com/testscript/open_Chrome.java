e79b2796dc5242efd4b59b7de05efcf9dc16972b
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2021-01-20 13:03:43 +0530

    Update open_Chrome.java

--- src/test/java/com/testscript/open_Chrome.java
+++ src/test/java/com/testscript/open_Chrome.java
@@ -4,13 +4,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 import com.base.BASE_Class;
 
 public class open_Chrome extends BASE_Class {
 	
+          	public static WebDriver driver;
 
 	// static String Url="https://www.google.com/";
 	@org.testng.annotations.Test
@@ -24,8 +25,9 @@ public class open_Chrome extends BASE_Class {
 	         // driver=new ChromeDriver(options);
 		
 		driver = new ChromeDriver();
-          
-		driver.navigate().to("https://www.google.com/");
+                driver.get("http://www.google.com");
+
+		//driver.navigate().to("https://www.google.com/");
 
 		// launchBrowser("https://mail.google.com/mail/u/0/#inbox");
        // driver.navigate().to("https://mail.google.com/mail/u/0/#inbox");
