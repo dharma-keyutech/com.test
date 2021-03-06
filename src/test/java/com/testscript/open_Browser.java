4d16e569af142c95be6cd002806d217181ba1d3a
dharma-keyutech <58975085+dharma-keyutech@users.noreply.github.com>
2021-03-06 18:20:29 +0530

    Update open_Browser.java

--- src/test/java/com/testscript/open_Browser.java
+++ src/test/java/com/testscript/open_Browser.java
@@ -2,7 +2,7 @@ package com.testscript;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
